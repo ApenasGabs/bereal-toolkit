612ca816f82d3c33f96b24f7701fe970d6f68aeb
Gabriel Rodrigues <gabers357@gmail.com>
2025-02-10 02:10:09 -0300

    altera o arquivo package.json para mudar o ponto de entrada e renomeia script de build, desabilita plugin do Electron no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import electron from "vite-plugin-electron";
 
 export default defineConfig({
   plugins: [
     react(),
-    electron({
-      entry: "packages/electron-core/src/main.ts",
-    }),
+    // electron({
+    //   entry: "packages/electron-core/src/main.ts",
+    // }),
   ],
   build: {
     outDir: "dist/react",
